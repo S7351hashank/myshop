1c9cedcf972450d3719250f7991124f9b0849d26
Shashank Kumar <sachinkumar46669@gmail.com>
2024-07-06 21:00:47 +0530

    some change12

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -159,7 +159,7 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite:"none",
+        sameSite:"None",
         secure:true,
       }
       res.cookie("token", token, options).status(200).json({
